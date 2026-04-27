9da7352ad19145d399f2673ca69980857a5fc36d
resourge <jdaoliveiraprograming@hotmail.com>
2026-04-27 11:09:41 +0100

    feat(config): more rules

--- src/config.ts
+++ src/config.ts
@@ -134,7 +134,11 @@ export default defineConfig([
 			'@stylistic/object-property-newline': ['error', {
 				allowAllPropertiesOnSameLine: false
 			}],
-			'@stylistic/operator-linebreak': ['error', 'before'],
+			'@stylistic/operator-linebreak': ['error', 'before', {
+				overrides: {
+					'=': 'after' 
+				} 
+			}],
 			'@stylistic/padded-blocks': ['error', 'never'],
 			'@stylistic/semi': ['error', 'always'],
 			'@stylistic/space-before-function-paren': 'off',
@@ -249,9 +253,67 @@ export default defineConfig([
 			"unicorn/prefer-number-properties": "off",
 			"unicorn/no-nested-ternary": "off",
 			"unicorn/consistent-function-scoping": "off",
+			"unicorn/no-null": "off",
 
 			'no-restricted-syntax': 'off',
 			'react-hooks/refs': 'off',
+
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					customGroups: [                      
+						{                                
+							elementNamePattern: {
+								pattern: '^react+(|-native)'
+							},
+							groupName: 'react',
+							order: 'asc'
+						}                            
+					],
+					fallbackSort: {
+						order: 'asc',
+						type: 'natural' 
+					},
+					groups: [
+						'react',
+						['value-builtin', 'type-builtin', 'value-external', 'type-external'],
+						['value-internal', 'type-internal'],
+						['value-parent', 'type-parent'],
+						['value-sibling', 'type-sibling'], 
+						['value-index', 'type-index']
+					],
+					ignoreCase: false,
+					internalPattern: ['^~/.+', '^@/.+', '^#.+', '^src/.+'],
+					newlinesBetween: 1,
+					order: 'asc',
+					type: 'alphabetical'
+				}
+			],
+
+			'perfectionist/sort-modules': [
+				'error',
+				{
+					groups: [
+						'declare-enum',
+						'export-enum',
+						'enum',
+						['interface', 'type'],
+						['declare-interface', 'declare-type'],
+						['export-interface', 'export-type'],
+						'declare-function',
+						'export-function',
+						'function',
+						'declare-class',
+						'export-class',
+						'class'
+					],
+					newlinesBetween: 1,
+					newlinesInside: 1,
+					order: 'asc',
+					type: 'alphabetical'
+				}
+			],
+			'unicorn/no-thenable': 'off'
 		}
 	}
 ]);
